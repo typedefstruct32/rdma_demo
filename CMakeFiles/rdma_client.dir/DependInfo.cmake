26912d67e644942af786bb79e4a82962ec189fa2
typedefstruct32 <420463729@qq.com>
2021-12-24 17:42:49 +0800

    while 1 for test

---
+++ CMakeFiles/rdma_client.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kyo/cpp/rdma-example/src/rdma_client.c" "/home/kyo/cpp/rdma-example/CMakeFiles/rdma_client.dir/src/rdma_client.c.o"
+  "/home/kyo/cpp/rdma-example/src/rdma_common.c" "/home/kyo/cpp/rdma-example/CMakeFiles/rdma_client.dir/src/rdma_common.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "/home/atr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
